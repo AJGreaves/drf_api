79e1910c7af972206bd2c135d16d51c304351835
A Greaves <anna.j.greaves@gmail.com>
2023-04-03 07:42:24 +0000

    Add React project repository to frontend folder

---
+++ frontend/src/pages/posts/PostPage.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import Comment from "../comments/Comment";
+
+import { axiosReq } from "../../api/axiosDefaults";
+
+import appStyles from "../../App.module.css";
+import Post from "./Post";
+
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { fetchMoreData } from "../../utils/utils";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+
+function PostPage() {
+  const { id } = useParams();
+
+  const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: post }, { data: comments }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
+        ]);
+        setPost({ results: [post] });
+        setComments(comments);
+      } catch (err) {}
+    };
+
+    handleMount();
+  }, [id]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <Post {...post.results[0]} setPosts={setPost} PostPage />
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+            dataLength={comments.results.length}
+            next={() => fetchMoreData(comments, setComments)}
+            hasMore={!!comments.next}
+            loader={<Asset spinner />}
+            children={comments.results.map((comment) => (
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
+            ))}
+          />
+          ) : currentUser ? (
+            <span>No comments yet, be the first one to comment!</span>
+          ) : (
+            <span>No comments… yet</span>
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
+}
+
+export default PostPage;
