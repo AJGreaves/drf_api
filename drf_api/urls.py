f9b1ded51c32cfb86d2ea6028ea6d629ecdb2384
A Greaves <anna.j.greaves@gmail.com>
2023-04-13 08:23:26 +0000

    Update import order in urls.py

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import logout_route
 from django.views.generic import TemplateView
+from .views import logout_route
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
